b58acd49e3f05612ca25c196604b22c78c38b6f7
LAPTOP-VP1F38CL\hokuy <hokuyoyuki@outlook.jp>
2025-01-30 01:35:52 +0900

    2章完成

--- lostconnection/src/main.jsx
+++ lostconnection/src/main.jsx
@@ -5,6 +5,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import GameCanvas from "./Prologue.jsx";
 import Chapter1 from "./Chapter1.jsx";
+import Chapter1_1 from "./Chapter1_1.jsx";
+import Chapter1_2 from "./Chapter1_2.jsx";
+import Chapter2 from "./Chapter2.jsx";
+import Chapter2_1 from "./Chapter2_1.jsx";
+import Chapter2_2 from "./Chapter2_2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,6 +18,11 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/prologue" element={<GameCanvas />} />
         <Route path="/chapter1" element={<Chapter1 />} />
+        <Route path="/chapter1_1" element={<Chapter1_1 />} />
+        <Route path="/chapter1_2" element={<Chapter1_2 />} />
+        <Route path="/chapter2" element={<Chapter2 />} />
+        <Route path="/chapter2_1" element={<Chapter2_1 />} />
+        <Route path="/chapter2_2" element={<Chapter2_2 />} />
       </Routes>
     </Router>
   </StrictMode>
