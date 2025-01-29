0b642e3a968d4d375f437acfde357bbe0abc45a9
LAPTOP-VP1F38CL\hokuy <hokuyoyuki@outlook.jp>
2025-01-30 05:13:17 +0900

    3章完成

--- lostconnection/src/main.jsx
+++ lostconnection/src/main.jsx
@@ -10,6 +10,9 @@ import Chapter1_2 from "./Chapter1_2.jsx";
 import Chapter2 from "./Chapter2.jsx";
 import Chapter2_1 from "./Chapter2_1.jsx";
 import Chapter2_2 from "./Chapter2_2.jsx";
+import Chapter3 from "./Chapter3.jsx";
+import Chapter3_1 from "./Chapter3_1.jsx";
+import Chapter3_2 from "./Chapter3_2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -23,6 +26,9 @@ createRoot(document.getElementById("root")).render(
         <Route path="/chapter2" element={<Chapter2 />} />
         <Route path="/chapter2_1" element={<Chapter2_1 />} />
         <Route path="/chapter2_2" element={<Chapter2_2 />} />
+        <Route path="/chapter3" element={<Chapter3 />} />
+        <Route path="/chapter3_1" element={<Chapter3_1 />} />
+        <Route path="/chapter3_2" element={<Chapter3_2 />} />
       </Routes>
     </Router>
   </StrictMode>
